276cf878a6e5a76e895a3f907895d966cee3bb7e
HeesungJeong <162422939+HeesungJeong@users.noreply.github.com>
2025-11-21 22:59:50 +0900

    Add licensing comments and modification details
    Added licensing and modification information to the file.

--- sampling.py
+++ sampling.py
@@ -1,3 +1,12 @@
+# This file includes code from the rectified-flow-pytorch project:
+# https://github.com/lucidrains/rectified-flow-pytorch
+# 
+# Original author: Phil Wang (lucidrains)
+# Licensed under the MIT License
+#
+# Modifications by: SKKU-SecLab
+# Date: 2025-11-21
+
 import torch
 import os
 from rectified_flow_pytorch import RectifiedFlow, ImageDataset, Unet, Trainer, load_siamese_and_ocr_models
@@ -22,4 +31,5 @@ trainer = Trainer(
 )
 
 trainer.sample_and_save_individual_images(checkpoint_path='./checkpoints/AT&T/checkpoint.70000.pt', result_path ='./sampling/AT&T', num_samples=16)
+
 
