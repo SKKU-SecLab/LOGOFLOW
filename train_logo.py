8d3cf97704012e1d86eb10b3b7c8bbb3d5e2907f
HeesungJeong <162422939+HeesungJeong@users.noreply.github.com>
2026-01-26 21:16:27 -0800

    Update author information and formatting in train_logo.py

--- train_logo.py
+++ train_logo.py
@@ -4,7 +4,7 @@
 # Original author: Phil Wang (lucidrains)
 # Licensed under the MIT License
 #
-# Modifications by: SKKU-SecLab
+# Modifications by: Anonymous
 # Date: 2025-11-21
 
 import torch
@@ -65,4 +65,5 @@ trainer.load(checkpoint_path)
 trainer.forward(start_step = 10000)'''
 
 trainer()
+
 
